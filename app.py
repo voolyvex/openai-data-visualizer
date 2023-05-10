fc4616abd6361c6b3e1d32bfadc5fc0e70ad9163
voolyvex <1joeknapp@gmail.com>
2023-05-10 13:41:40 -0500

    Refactor code, placing tableau functionality in a separate module and deleting unused files from build

--- app.py
+++ app.py
@@ -1,119 +1,18 @@
 import streamlit as st
-from dotenv import load_dotenv
-
-load_dotenv()
-
-from streamlit.runtime.uploaded_file_manager import UploadedFile
-
-import text_to_speech as tts
-from explainer import retrieve_data_label, retrieve_data_summary
-
-TTS_ENABLED = True
-
+from tableau import embed_dashboard
 
 def display_header() -> None:
     st.image("img/logo.jpg")
-    st.title("Welcome to AI Data Visualizer")
-    st.text("Just upload your data or copy and paste in the field below")
-    st.warning("Warning: uploaded files have precendence on copied and pasted data.")
-
-
-def display_widgets() -> tuple[UploadedFile, str]:
-    file = st.file_uploader("Upload your .csv file here.")
-    text = st.text_area("or copy and paste as text here (press Ctrl + Enter to send)")
-
-    if not (text or file):
-        st.error("Provide data with one of the options above.")
-
-    return file, text
-
-
-def retrieve_content_from_file(file: UploadedFile) -> str:
-    file_contents = file.getvalue()
-    if file.name.endswith(".csv"):
-        return file_contents.decode("ISO-8859-1")
-    elif file.name.endswith(".txt"):
-        return file_contents.decode("utf-8")
-    elif file.name.endswith(".db"):
-        # replace the following line with the appropriate database query
-        return "SELECT * FROM table_name"
-    else:
-        raise ValueError(f"Unsupported file type: {file.name}")
-
-
-
-
-def extract_code() -> str:
-    uploaded_script, pasted_code = display_widgets()
-
-    if uploaded_script:
-        return retrieve_content_from_file(uploaded_script)
-    return pasted_code or ""
-
-
-def choose_voice():
-    voices = tts.list_available_names()
-    return st.selectbox(
-        "Please choose one of the available voices.",
-        voices,
-    )
-
+    st.title("Welcome to Tableau Visualizer")
 
 def main() -> None:
     display_header()
+    tableau_url = "https://prod-useast-b.online.tableau.com/t/biworkspace/views/ExecutiveOverview/ExecutiveOverview?:origin=card_share_link&:embed=n"
+    tableau_options = {
+        "onFirstInteractive": "function () { workbook = viz.getWorkbook(); activeSheet = workbook.getActiveSheet(); }"
+    }
+    dashboard = embed_dashboard(tableau_url, tableau_options)
+    st.components.v1.html(dashboard, height=800)
 
-    selected_voice = choose_voice()
-
-    if data_to_explain := extract_code():
-        with st.spinner(text="Let me analyze it..."):
-            label = retrieve_data_label(code=data_to_explain)
-            summary = retrieve_data_summary(code=data_to_explain)
-            # insight = retrieve_data_insights(code=data_to_explain)
-            # visual = retrieve_data_visualization(code=data_to_explain)
-
-        with st.spinner(text="Give me a little more time, this data is complex..."):
-            tts.convert_text_to_mp3(
-                message=label, voice_name=selected_voice, mp3_filename="label.mp3"
-            )
-        with st.spinner(
-            text=(
-                "I created a label."
-                "I'm thinking about how to summarize this..."
-            )
-        ):
-            tts.convert_text_to_mp3(
-                message=summary,
-                voice_name=selected_voice,
-                mp3_filename="summary.mp3",
-            )
-
-        st.success("I completed my summary of the data.")
-        st.warning("Please enable audio...")
-
-        st.markdown(f"**Label:** {label}")
-        st.audio("label.mp3")
-
-        st.markdown(f"**Summary:** {summary}")
-        st.audio("summary.mp3")
-
-        # with st.spinner(
-        #     text=(
-        #         "I'm analyzing the data now..."
-        #     )
-        # ):
-        #     tts.convert_text_to_mp3(
-        #         message=insight,
-        #         voice_name=selected_voice,
-        #         mp3_filename="insight.mp3",
-        #     )
-        # st.success("Done. Here is my take on the data you provided.")
-
-        # st.markdown(f"**Insight:** {insight}")
-        # st.audio("insight.mp3")
-
-        # st.markdown(f"**Visualization:** {visual}")
-        
-        # st.altair_chart(visuals, use_container_width=False, theme="streamlit")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
---
+++ tableau.py
@@ -0,0 +1,25 @@
+import tableauserverclient as TSC
+import streamlit as st
+
+def embed_dashboard(tableau_url: str, tableau_options: dict) -> str:
+    tableau_auth = TSC.PersonalAccessTokenAuth(
+        st.secrets["tableau"]["token_name"],
+        st.secrets["tableau"]["personal_access_token"],
+        st.secrets["tableau"]["site_id"],
+    )
+    print(st.secrets["tableau"]["token_name"])
+    print(st.secrets["tableau"]["personal_access_token"])
+    print(st.secrets["tableau"]["site_id"])
+    server = TSC.Server(st.secrets["tableau"]["server_url"], use_server_version=True)
+    
+    with server.auth.sign_in(tableau_auth):
+        dashboard = f"""
+            <div class='tableauPlaceholder' id='tableauViz'></div>
+            <script>
+            var placeholderDiv = document.getElementById('tableauViz');
+            var url = '{tableau_url}';
+            var options = {tableau_options};
+            var viz = new tableau.Viz(placeholderDiv, url, options);
+            </script>
+        """
+        return dashboard
